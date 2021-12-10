2d8003010b68bf662d8fdab219a27dc56b39eedc
PoterPan <poterpan5466@gmail.com>
2021-12-11 01:07:24 +0800

    Initial Commit

---
+++ FCU_AutoPass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FCU_AutoPass
+//
+//  Created by Poter Pan on 2021/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FCU_AutoPass/FCU_AutoPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCU_AutoPassApp.swift
+//  FCU_AutoPass
+//
+//  Created by Poter Pan on 2021/12/11.
+//
+
+import SwiftUI
+
+@main
+struct FCU_AutoPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
